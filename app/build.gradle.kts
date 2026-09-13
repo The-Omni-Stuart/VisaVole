b34effbde87ebda60eb607327656d4fc99ce2180
cbk-res <cbk-res@protonmail.com>
2026-09-13 17:58:16 +0100

    Add GitHub Actions APK builds and alpha version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 28
         targetSdk = 37
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0-alpha01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
